3325e08b20e125236eec32c9df94f9bd5ee28696
morekamlesh <kamleshrm007@gmail.com>
2023-05-26 03:29:21 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sansserif: ["sans-serif", "sans-serif"],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue300: "#4b94ed",
+        lightBlue500: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76",
+        netback:"#00081d",
+        listblack:"#0f1118",
+        hotred:"#ff0000",
+        border:"#252238",
+        divback:"#132144",
+        grey001:"rgba(255,255,255,0.7)",
+
+      },
+    },
+  },
+  plugins: [],
+};
